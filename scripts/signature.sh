0932e5c904992e806229b250f05707dfd42e5538
Robert Banderby <rbanderby@gmail.com>
2026-06-13 16:02:57 +0200

    Complete part 1: setup and signature scripts

---
+++ scripts/signature.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "============================================================"
+echo "Name:      Vulkan"
+echo "Email:     robban940904@student.jenseneducation.se"
+echo "Timestamp: $(date '+%Y-%m-%d %H:%M:%S')"
+echo "Hostname:  $(hostname)"
+echo "IP:        $(hostname -I | awk '{print $1}')"
+echo "============================================================"
